8e09f7d21790fb619c77f8eecb2d3bc99c9ce29a
Khanh <nguyenvietnamkhanh@gmail.com>
2012-06-23 10:55:38 +0700

    update html code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+
+  # antrai
+  # congdung
+  # congnghiep
+  # hoakieng
+  # lienhe
+  # lua 
+  # MicroBoost
+  # nganngay
+  # npk6_19_0
+  # raucu
+  # raumau
+  # sanpham
+  # super3_18_18
+  # super6_14_6
+  # supercal
+  # supperfish
+  # superhume
+  # supper10_8_8
+  # ykienchuyengia
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,23 @@
 Phuhao::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match '/antrai' => 'home#antrai'
+  match '/congdung' => 'home#congdung'
+  match '/congnghiep' => 'home#congnghiep'
+  match '/hoakieng' => 'home#hoakieng'
+  match '/lienhe' => 'home#lienhe'
+  match '/lua' => 'home#lua'
+  match '/MicroBoost' => 'home#MicroBoost'
+  match '/nganngay' => 'home#nganngay'
+  match '/npk6_19_0' => 'home#npk6_19_0'
+  match '/raucu' => 'home#raucu'
+  match '/raumau' => 'home#raumau'
+  match '/sanpham' => 'home#sanpham'
+  match '/super3_18_18' => 'home#super3_18_18'
+  match '/super6_14_6' => 'home#super6_14_6'
+  match '/supercal' => 'home#supercal'
+  match '/supperfish' => 'home#supperfish'
+  match '/superhume' => 'home#superhume'
+  match '/supper10_8_8' => 'home#supper10_8_8'
+  match '/ykienchuyengia' => 'home#ykienchuyengia'
+  match '/index' => 'home#index'
+  root :to => 'home#index'
 end
