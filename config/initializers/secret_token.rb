2c6e0df6425a9a04c309cc410c06fdd0b309198a
Khanh <nguyenvietnamkhanh@gmail.com>
2012-06-23 10:35:19 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phuhao::Application.config.secret_token = '8382cf05cd06255c79556102672788a0a1a70ed66b03f2307a3514112be03c90e7dfadd0cba928509eea2b27cfea2295b87ca89db537ccfb9d84337723f47090'
