529576e74a789d406de87dde1b6e273b4f0c9f42
Khanh <nguyenvietnamkhanh@gmail.com>
2015-12-16 15:13:13 +0700

    Minor change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+    $("#single_1").fancybox({
+        helpers: {
+            title : {
+                type : 'float'
+            }
+        }
+    });
+
+    $("#single_2").fancybox({
+        openEffect	: 'elastic',
+        closeEffect	: 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
